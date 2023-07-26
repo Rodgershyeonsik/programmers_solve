fa5218405534badd8ed40e74f88e303c7c244280
HEEJI YUN <yhj8147e@gmail.com>
2023-07-26 14:31:25 +0900

    햄버거 만들기 시간 초과로 실패2...

--- demo/src/main/java/com/example/demo/level1/Hamburger.java
+++ demo/src/main/java/com/example/demo/level1/Hamburger.java
@@ -1,8 +1,5 @@
 package com.example.demo.level1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Hamburger {
     public static void main(String[] args) {
         int[] ingrdnts = {2, 1, 1, 2, 3, 1, 2, 3, 1};
@@ -14,33 +11,26 @@ public class Hamburger {
 class SolHam {
     public int solution(int[] ingredient) {
         int answer = 0;
-        // 빵:1 야채:2 고기:3
-        // 배열을 검사해서 1231 순서면 햄버거 포장
-        // 일단 가장 먼저 나오는 1을 찾는다.
-        List<Integer> ingredientList = new ArrayList<>();
-        for (int ingre: ingredient) {
-            ingredientList.add(ingre);
+        StringBuilder ingredientString = new StringBuilder();
+        // int 배열을 string으로 변환
+        for (int i = 0; i < ingredient.length; i++) {
+            ingredientString.append(ingredient[i]);
         }
-        int firstBunIdx = ingredientList.indexOf(1);
-        while (ingredientList.size() >= 4) {
-            // 네번째 빵이 나올 위치가 ingredient.length - 1보다 크면 햄버거 포장 못함.
-            if(firstBunIdx + 3 > ingredientList.size() - 1) {
+        // string 길이가 4 이상일 때까지 반복
+        while(ingredientString.length() >= 4 ) {
+            System.out.println("재료 문자열: " + ingredientString);
+            // 문자열이 1231 포함하면
+            int firstHam = ingredientString.toString().indexOf("1231");
+            System.out.println("햄버거 위치: " + firstHam);
+            if(firstHam < 0) {
                 return answer;
             }
-            if(ingredientList.get(firstBunIdx+1).equals(2) &&
-                    ingredientList.get(firstBunIdx+2).equals(3) &&
-                    ingredientList.get(firstBunIdx+3).equals(1)) {
-                answer++;
-                // 햄버거 포장한 재료 치우기
-                ingredientList.remove(firstBunIdx);
-                ingredientList.remove(firstBunIdx);
-                ingredientList.remove(firstBunIdx);
-                ingredientList.remove(firstBunIdx);
-                // 빵 위치 다시 찾기
-                firstBunIdx = ingredientList.indexOf(1);
-            } else {
-                firstBunIdx++;
-            }
+            answer++;
+            StringBuilder tmpString = new StringBuilder();
+            tmpString.append(ingredientString.substring(0, firstHam));
+            tmpString.append(ingredientString.substring(firstHam+4, ingredientString.length()));
+            ingredientString = tmpString;
+            System.out.println("새로운 재료 문자열: " + ingredientString);
         }
         return answer;
     }
